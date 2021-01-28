3275fc712381cf39b06ffe3683c8eff97ce7aec0
avrm <avrmeduard@gmail.com>
2021-01-28 19:31:22 +0200

    return type modified. we return a string token

--- src/main/java/com/avrm/springblog/controller/AuthController.java
+++ src/main/java/com/avrm/springblog/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.avrm.springblog.dto.LoginRequest;
 import com.avrm.springblog.dto.RegisterRequest;
 import com.avrm.springblog.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +27,8 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity logIn(@RequestBody LoginRequest loginRequest) {
-        authService.logIn(loginRequest);
-        return ResponseEntity.ok(HttpStatus.OK);
+    public String logIn(@RequestBody LoginRequest loginRequest) {
+        return authService.logIn(loginRequest);
     }
 
 }
