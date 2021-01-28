b996c3ea09b2cb00f8bd91e0b635c08d2c9040ed
avrm <avrmeduard@gmail.com>
2021-01-28 22:49:25 +0200

    save the post to database

--- src/main/java/com/avrm/springblog/controller/PostController.java
+++ src/main/java/com/avrm/springblog/controller/PostController.java
@@ -1,5 +1,8 @@
 package com.avrm.springblog.controller;
 
+import com.avrm.springblog.dto.PostDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/post")
 public class PostController {
 
+    @PostMapping
+    public void createPost(@RequestBody PostDto postDto) {
 
+    }
 }
