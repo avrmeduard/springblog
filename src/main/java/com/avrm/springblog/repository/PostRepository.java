b6c6f7db6b8fffb1b39a43c9885180666e71ca94
avrm <avrmeduard@gmail.com>
2021-01-28 22:49:04 +0200

    PostRepository created

---
+++ src/main/java/com/avrm/springblog/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.avrm.springblog.repository;
+
+import com.avrm.springblog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
