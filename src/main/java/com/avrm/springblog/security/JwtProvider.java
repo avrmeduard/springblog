bafa1c53132fcf7f12d1f93d3437e2437c5b34a9
avrm <avrmeduard@gmail.com>
2021-01-28 13:41:27 +0200

    we create a single key JSON web token and reuse it

--- src/main/java/com/avrm/springblog/security/JwtProvider.java
+++ src/main/java/com/avrm/springblog/security/JwtProvider.java
@@ -7,8 +7,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.security.Key;
+
 @Service
 public class JwtProvider {
+
+    private Key key;
+
+    // we create at the time service start and we reuse the same key every time we generate a JSON token
+    @PostConstruct
+    public void init() {
+        key = Keys.secretKeyFor(SignatureAlgorithm.HS512); // create a key json web token special algorithm
+    }
 
     // takes the authenticate obj as parameter to generate the jwt token
     public String generateToken(Authentication authentication) {
@@ -18,7 +29,7 @@ public class JwtProvider {
         // we invoke the builder method of the class
         return Jwts.builder()
                 .setSubject(principal.getUsername())
-                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS512)) // create a key json web token special algorithm
+                .signWith(key)
                 .compact();
     }
 }
