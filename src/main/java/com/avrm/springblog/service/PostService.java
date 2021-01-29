fc5522dedf9228a7afa0ed7bdacd30a0664bc0d9
avrm <avrmeduard@gmail.com>
2021-01-29 10:17:21 +0200

    create Service post method added

--- src/main/java/com/avrm/springblog/service/PostService.java
+++ src/main/java/com/avrm/springblog/service/PostService.java
@@ -1,6 +1,7 @@
 package com.avrm.springblog.service;
 
 import com.avrm.springblog.dto.PostDto;
+import com.avrm.springblog.exception.PostNotFoundException;
 import com.avrm.springblog.model.Post;
 import com.avrm.springblog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 
 @Service
 public class PostService {
@@ -18,14 +23,20 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    public void createPost(PostDto postDto) {
+    public Post mapFromPostToDto(PostDto postDto) {
         Post post = new Post();
-        post.setTitle(post.getTitle());
+        post.setTitle(postDto.getTitle());
         post.setContent(postDto.getContent());
         // we need to find the current user fom AuthService other while throw runtime exception
-        User username = authService.getCurrentUser().orElseThrow(() -> new IllegalArgumentException("No user login"));
-        post.setUsername(username.getUsername());
+        User loggedInUser = authService.getCurrentUser().orElseThrow(() -> new IllegalArgumentException("No user login"));
+        post.setUsername(loggedInUser.getUsername());
         post.setCreatedOn(Instant.now());
+        post.setUpdatedOn(Instant.now());
+        return post;
+    }
+
+    public void createPost(PostDto postDto) {
+        Post post = mapFromPostToDto(postDto);
         postRepository.save(post);
     }
 
